5da7415134e91d386e42ce164ced864a4b00dafc
yoga-nugroho129 <yoga.nugroho129@gmail.com>
2019-11-30 23:33:09 +0700

    finishing banner & about section

--- js/script.js
+++ js/script.js
@@ -6,11 +6,29 @@ $(document).ready(function() {
         if (scrollValue > 70) {
             $('#home-nav-scroll').removeClass('bg-transparent');
             $('#home-nav-scroll').addClass('scrolled-nav');
+            $('#red-text').removeClass('media-text');
+            $('#red-text').addClass('rantau-text');
         } else {
             $('#home-nav-scroll').removeClass('scrolled-nav');
+            $('#red-text').addClass('media-text');
         }
 
-        console.log(scrollValue);
+        if (scrollValue < 300) {
+            $('#home-pos').addClass('nav-link-active');
+        }
+        // console.log(scrollValue);
+
+    });
+
+    var screenWidth = $(window).width();
+    $(window).on('resize', function() {
+        if (screenWidth < 1091) {
+            $('#home-nav-scroll').addClass('small-screen-nav');
+            $('#home-nav-scroll').removeClass('bg-transparent');
+        } else {
+            $('#home-nav-scroll').removeClass('small-screen-nav');
+            $('#home-nav-scroll').addClass('bg-transparent');
+        }
     });
 
 });
